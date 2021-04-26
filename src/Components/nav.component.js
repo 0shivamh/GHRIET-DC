110b537c5a559cfbc3dd5f33cb6dfd0e1a6755d7
aishivam <shivamhande0000@gmail.com>
2021-04-26 20:09:51 +0530

    navbar and icon updated

--- src/Components/nav.component.js
+++ src/Components/nav.component.js
@@ -1,44 +1,37 @@
 import React, { useEffect } from 'react'
-
+import icon from "../img/open.png"
 const Nav=()=> {
         return(
 
             <div>
-                <nav className="navbar fixed-top navbar-expand-lg ">
-                    <div className="container-fluid" >
-                        <a className="navbar-brand" href="#">GHRIET-DC  
-                        <img src="" alt="" width="30" height="24"/>
-                        </a>
-                        <button id="navbtn navbtn1" className="navbar-toggler" type="button"  data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <i className="fa fa-bars" aria-hidden="true"></i>
+               <nav className="navbar fixed-top navbar-expand-lg navbar-light shadow-lg p-3 mb-5 bg-body rounded">
+                    <div className="container-fluid">
+                    <a class="navbar-brand" href="/">
+                        <img src={icon} className="img-fluid vert-move" alt="dev-fellow" width="30" height="24" class="d-inline-block align-text-top"/>
+                        Dev-Felllow
+                    </a>
+         
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                         </button>
-                    
-                        <div className="justify-content-end collapse navbar-collapse " id="navbarSupportedContent">
-                        
-                        <ul className="navbar-nav"  >
+                        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav ms-auto">
                             <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <a className="nav-link active" aria-current="page" href="/">Home</a>
                             </li>
                             <li className="nav-item">
-                            <a className="nav-link" aria-current="page" href="#">About</a>
-                            </li>
-                            <li className="nav-item dropdown ">
-                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul className="dropdown-menu animate__animated animate__fadeIn" aria-labelledby="navbarDropdown">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
+                            <a className="nav-link" href="#about">About</a>
                             </li>
                             <li className="nav-item">
-                            <a className="nav-link" aria-current="page" href="#">Blog</a>
+                            <a className="nav-link" href="#">Team</a>
                             </li>
                             <li className="nav-item">
-                            <a className="nav-link" aria-current="page" href="#">None</a>
+                            <a className="nav-link" href="#pro">Projects</a>
                             </li>
-                        </ul>    
+                            <li className="nav-item">
+                            <a className="nav-link" href="#event">Events</a>
+                            </li>
+                        </ul>
                         </div>
                     </div>
                     </nav>
