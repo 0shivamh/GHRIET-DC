075d037541ebc17f816123f78ec2c75a9e5186ba
aishivam <shivamhande0000@gmail.com>
2021-05-02 09:08:16 +0530

    Join and community added

---
+++ src/backend/community.backend.js
@@ -0,0 +1,24 @@
+
+const Community=()=>{
+
+    return(
+        <>
+            
+            <div className="modal fade cmodal" id="Community" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="CommunityLabel" aria-hidden="true">
+            <div className="modalc modal-dialog ">
+                <div className="modal-content">
+                <div className="modal-header">
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div className="">
+                <iframe className="card-body" height="500px" src="https://discord.com/widget?id=838248616597913600&theme=dark" allowtransparency="true" frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+                </div>
+                </div>
+            </div>
+            </div>
+</>
+    )
+
+
+}
+export default Community;
